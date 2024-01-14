15566db10d032fb34a7ac8e0f46eb8e53e6cb3dd
Jury Gregorio <jury.romblondev@outlook.com>
2024-01-14 23:06:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away❤️</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <h2>Packing List</h2>
+    </div>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
