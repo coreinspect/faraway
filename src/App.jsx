f6ee2d33740bdfa4c91f855332e520a113bf6a64
Jury Gregorio <jury.romblondev@outlook.com>
2024-01-25 00:39:24 +0800

    Updated Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -13,19 +21,80 @@ function Logo() {
   return <h1>🌴Far Away❤️</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // if there is no description, don't add
+    if (!description) {
+      return;
+    }
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    console.log(newItem);
+
+    //set the state to the default after adding new Item
+    setDescription('');
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          console.log(quantity);
+          setQuantity(Number(e.target.value));
+        }}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Add an item"
+        //the default value is "" but change when user put something
+        // the value is set to the state
+        value={description}
+        // the value of the input is set to base on the state and target value
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
   return (
     <div className="list">
-      <h2>Packing List</h2>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+Item.propTypes = {
+  item: PropTypes.shape({
+    quantity: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    packed: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
 function Stats() {
   return (
     <footer className="stats">
